f8d36063bfb2d05aae813057e6103619d7710e74
OngTK <dhdxorud@gmail.com>
2025-07-03 00:25:35 +0900

    250703_1225

--- 프로젝트/01_공통_로그인/login.js
+++ 프로젝트/01_공통_로그인/login.js
@@ -25,7 +25,7 @@ function testbtn() {
         return;
     }else if (member.Name === "admin"){
         alert('관리자임');
-        location.href = '../11_admin_직원관리/11_member.html';    //관리자용
+        location.href = '../11_admin_직원관리/test/11_member.html';    //관리자용
     }else {
 
     alert(`${member.Name}님, 어서오세요!`);
