e47503b116cf83952806c7a0429a9f577e8138ed
OngTK <dhdxorud@gmail.com>
2025-07-02 14:50:00 +0900

    250702_1449

--- 프로젝트/12_admin_근태이력/12_attendAdmin.js
+++ 프로젝트/12_admin_근태이력/12_attendAdmin.js
@@ -50,7 +50,7 @@ function pageNumber() {
     let url = new URLSearchParams(location.search);
     let page = Number(url.get('page'))
 
-    let pageDown = page - 1 ;
+    let pageDown = page - 1;
     let pageUp = page + 1;
 
     let pageqty = pageQTY()
@@ -61,6 +61,7 @@ function pageNumber() {
             </li>`
     for (let i = 1; i <= pageqty; i++) {
         html += `<li class="page-item"><a class="page-link" href="12_attendAdmin.html?page=${i}">${i}</a></li>`
+    }
     html += `<li class="page-item">
                 <a class="page-link" href="12_attendAdmin.html?page=${pageUp}" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
@@ -68,7 +69,6 @@ function pageNumber() {
             </li>`
     pagination.innerHTML = html
 }
-
 
 //근태이력 조회====================================================================================
 
@@ -202,22 +202,34 @@ function AttendaceViewModal(attendID) {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div>사번 <input type="number" value="${member.memberID}" disabled/> </div> 
-                    <div>이름 <input type="text" value="${member.Name}" disabled/></div> 
-                    <div>부서 <input type="text" value="${changeDepartName(member.departID)}" disabled/></div>
-                    <div>직급 <input type="text" value="${changePositionName(member.posiID)}" disabled/></div>
-                    <div>날짜 <input type="date" value="${attendace.date}" disabled/></div>
-                    <div>출근시간 
-                        <input type="time" value="${attendace.attentTime}" />
+                    <div>  
+                     <label for="recipient-name" class="col-form-label">사번</label>
+                     <input type="number" value="${member.memberID}" class="form-control" id="recipient-name" disabled/> </div> 
+                    <div>  
+                     <label for="recipient-name" class="col-form-label">이름</label>
+                    <input type="text" value="${member.Name}" class="form-control" id="recipient-name" disabled/></div> 
+                    <div>  
+                     <label for="recipient-name" class="col-form-label">부서</label>
+                    <input type="text" value="${changeDepartName(member.departID)}" class="form-control" id="recipient-name" disabled/></div>
+                    <div>  
+                     <label for="recipient-name" class="col-form-label">직급</label>
+                    <input type="text" value="${changePositionName(member.posiID)}"class="form-control" id="recipient-name"  disabled/></div>
+                    <div>  
+                     <label for="recipient-name" class="col-form-label">날짜</label>
+                    <input type="date" value="${attendace.date}"class="form-control" id="recipient-name"  disabled/></div>
+                    <div>  
+                     <label for="recipient-name" class="col-form-label">출근시간</label>
+                        <input type="time" value="${attendace.attentTime}" class="form-control" id="recipient-name" />
                     </div>
-                    <div>퇴근시간
-                        <input type="time" value="${attendace.leaveTime}"/>
-                        <button type="button" class="btn btn-primary" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" onclick=valueReset(${attendID})>시간 초기화</button>
+                    <div>
+                     <label for="recipient-name" class="col-form-label">퇴근시간</label>
+                        <input type="time" value="${attendace.leaveTime}" class="form-control" id="recipient-name"/>
+                        <button type="button" class="btn btn-primary reset-button" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" onclick=valueReset(${attendID})>시간 초기화</button>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
-                    <button type="button" class="btn btn-primary" onclick="AttendaceUpdate(${attendID})" data-bs-dismiss="modal">저장</button>
+                    <button type="button" class="btn btn-primary " onclick="AttendaceUpdate(${attendID})" data-bs-dismiss="modal">저장</button>
                 </div>`
             document.querySelector(".modal-content").innerHTML = html
         }
